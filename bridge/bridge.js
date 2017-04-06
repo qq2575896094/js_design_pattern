a3c98aa2a457bb54e9cde395c309804a51234255
qq2575896094 <2575896094@qq.com>
2017-04-06 19:53:11 +0800

    提交人:qq2575896094 时间:2017年04月06日19:52:41 版本:设计模式 1.0.0 描述:添加桥接模式和组合模式

--- bridge/bridge.js
+++ bridge/bridge.js
@@ -24,9 +24,77 @@ function changeColor(dom, color, bg) {
 }
 
 var spans = document.getElementsByClassName("span");
-spans[0].addEventListener("mouseover",function () {
-    changeColor(this,"red","#ddd");
-},false);
+spans[0].addEventListener("mouseover", function () {
+    changeColor(this, "red", "#ddd");
+}, false);
 
 
-//多元对象
+//多元对象----多维变量类
+//运动单元
+function Speed(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Speed.prototype.run = function () {
+    console.log("运动起来");
+};
+
+//着色单元
+function Color(cl) {
+    this.color = cl;
+}
+Color.prototype.draw = function () {
+    console.log("绘制色彩");
+};
+
+//变形单元
+function Shape(sp) {
+    this.shape = sp;
+}
+Shape.prototype.change = function () {
+    console.log("改变形状");
+};
+
+//说话单元
+function Speek(wd) {
+    this.word = wd;
+}
+Speed.prototype.say = function () {
+    console.log("书写字体");
+};
+
+//=================创建一个球类,并且它可以运动,可以着色=========================
+function Ball(x, y, c) {
+    //实现运动单元
+    this.speed = new Speed(x, y);
+    //实现着色单元
+    this.color = new Color(c);
+}
+Ball.prototype.init = function () {
+    //实现运动
+    this.speed.run();
+    //实现着色
+    this.color.draw();
+};
+
+//=================创建一个人物类,他可以运动以及说话=========================
+function People(x, y, f) {
+    this.speed = new Speed(x, y);
+    this.speek = new Speek(f);
+}
+People.prototype.init = function () {
+    this.speed.run();
+    this.speek.say();
+};
+
+//=================创建一个精灵类,他可以运动,着色以及改变形状=========================
+function Spirite(x, y, c, s) {
+    this.speed = new Speed(x, y);
+    this.color = new Color(c);
+    this.shape = new Shape(s);
+}
+Spirite.prototype.init = function () {
+    this.speed.run();
+    this.color.draw();
+    this.shape.change();
+}
